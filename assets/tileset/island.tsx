949032ec8889110abb3973e9cc1839c9bb90195d
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-05-17 07:32:44 -0300

    Fixing typo on tsx file

--- assets/tileset/island.tsx
+++ assets/tileset/island.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.2" name="island" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="insland.png" width="320" height="320"/>
+ <image source="island.png" width="320" height="320"/>
  <tile id="1">
   <properties>
    <property name="description" value="Sand"/>
