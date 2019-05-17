16234345f98bfa53893c8c4e8197b2571711523a
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-05-17 09:02:35 -0300

    movable class. #'direction-velocity helper function

--- src/util.lisp
+++ src/util.lisp
@@ -10,6 +10,9 @@
 
 (defparameter *assets-path* (asdf:system-relative-pathname :deserted "assets/"))
 
+(alexandria:define-constant +diagonal-unit+ 0.70710677
+  :test #'=)
+
 ;;; Rendering
 
 (defgeneric render (object)
@@ -32,3 +35,50 @@
 (defmethod (setf position-of) ((vec vec2) (this positionable))
   (setf (x (position-of this)) (x vec)
         (y (position-of this)) (y vec)))
+
+;;; Movement
+
+(defun direction-velocity (direction)
+  (cond
+    ((eql direction 'south)
+     (vec2 0 -1))
+    ((eql direction 'southwest)
+     (vec2 (- +diagonal-unit+) (- +diagonal-unit+)))
+    ((eql direction 'southeast)
+     (vec2 +diagonal-unit+ (- +diagonal-unit+)))
+    ((eql direction 'north)
+     (vec2 0 1))
+    ((eql direction 'northwest)
+     (vec2 (- +diagonal-unit+) +diagonal-unit+))
+    ((eql direction 'northeast)
+     (vec2 +diagonal-unit+ +diagonal-unit+))
+    ((eql direction 'east)
+     (vec2 1 0))
+    ((eql direction 'west)
+     (vec2 -1 0))))
+
+(defgeneric move (movable delta-time))
+
+(defclass movable (positionable)
+  ((velocity :initform (vec2 0 0) :accessor velocity-of)
+   (direction :initform 'south :accessor direction-of)
+   (moving-p :initform nil :accessor moving-p)))
+
+(defmethod move ((this movable) delta-time)
+  (with-slots (direction velocity position) this
+    (let* ((direction-speed (direction-velocity direction))
+	   (real-speed (mult
+			(mult velocity delta-time)
+			direction-speed))
+	   (floor-or-ceiling-x
+	    (if (> (x direction-speed) 0)
+		#'ceiling
+		#'floor))
+	   (floor-or-ceiling-y
+	    (if (> (y direction-speed) 0)
+		#'ceiling
+		#'floor))
+	   (real-position (add position real-speed)))
+      (setf position (vec2
+		      (funcall floor-or-ceiling-x (x real-position))
+		      (funcall floor-or-ceiling-y (y real-position)))))))
