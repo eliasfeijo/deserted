f9dfeba1736c337e8339b692c2924bc36ccd5d08
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-05-17 10:04:35 -0300

    Fixing indentation

--- src/tile.lisp
+++ src/tile.lisp
@@ -18,83 +18,83 @@
 
 (defun position-of-tile-on-tileset (tileset-index tileset)
   (let* ((row-count (/ (tile-count-of tileset) (columns-of tileset)))
-	 (tileset-height (* (tile-height-of tileset) row-count))
-	 (current-row (floor (/ tileset-index row-count)))
-	 (position-x
-	  (*
-	   (tile-width-of tileset)
-	   (mod tileset-index (columns-of tileset))))
-	 (position-y
-	  (- tileset-height (* (+ current-row 1) (tile-height-of tileset)))))
+         (tileset-height (* (tile-height-of tileset) row-count))
+         (current-row (floor (/ tileset-index row-count)))
+         (position-x
+          (*
+           (tile-width-of tileset)
+           (mod tileset-index (columns-of tileset))))
+         (position-y
+          (- tileset-height (* (+ current-row 1) (tile-height-of tileset)))))
     (vec2 position-x position-y)))
 
 (defun make-tile (gid map)
   (let ((flipped-horizontally-p
-	 (not (equal (logand gid +flipped-horizontally-flag+) 0)))
-	(flipped-vertically-p
-	 (not (equal (logand gid +flipped-vertically-flag+) 0)))
-	(flipped-diagonally-p
-	 (not (equal (logand gid +flipped-diagonally-flag+) 0)))
-	(gid-without-flags
-	 (logand gid (lognot (logior +flipped-horizontally-flag+ +flipped-vertically-flag+ +flipped-diagonally-flag+)))))
+         (not (equal (logand gid +flipped-horizontally-flag+) 0)))
+        (flipped-vertically-p
+         (not (equal (logand gid +flipped-vertically-flag+) 0)))
+        (flipped-diagonally-p
+         (not (equal (logand gid +flipped-diagonally-flag+) 0)))
+        (gid-without-flags
+         (logand gid (lognot (logior +flipped-horizontally-flag+ +flipped-vertically-flag+ +flipped-diagonally-flag+)))))
     (loop for tileset across (tilesets-of map) do
-	 (if (<= (first-gid-of tileset) gid-without-flags)
-	     (let ((tileset-index (- gid-without-flags (first-gid-of tileset))))
-	       (return (make-instance
-			'tile
-			:tileset-index tileset-index
-			:tileset tileset
-			:flipped-horizontally-p flipped-horizontally-p
-			:flipped-vertically-p flipped-vertically-p
-			:flipped-diagonally-p flipped-diagonally-p
-			:position-on-tileset (position-of-tile-on-tileset tileset-index tileset))))))))
+         (if (<= (first-gid-of tileset) gid-without-flags)
+             (let ((tileset-index (- gid-without-flags (first-gid-of tileset))))
+               (return (make-instance
+                        'tile
+                        :tileset-index tileset-index
+                        :tileset tileset
+                        :flipped-horizontally-p flipped-horizontally-p
+                        :flipped-vertically-p flipped-vertically-p
+                        :flipped-diagonally-p flipped-diagonally-p
+                        :position-on-tileset (position-of-tile-on-tileset tileset-index tileset))))))))
 
 (defmethod render ((this tile))
   (with-slots
-	(tileset
-	 position-on-tileset
-	 flipped-horizontally-p
-	 flipped-vertically-p
-	 flipped-diagonally-p
-	 position
-	 tileset-image)
+        (tileset
+         position-on-tileset
+         flipped-horizontally-p
+         flipped-vertically-p
+         flipped-diagonally-p
+         position
+         tileset-image)
       this
     (let ((real-position position)
-	  (tile-width (tile-width-of tileset))
-	  (tile-height (tile-height-of tileset)))
+          (tile-width (tile-width-of tileset))
+          (tile-height (tile-height-of tileset)))
       (with-pushed-canvas ()
-	(if (and flipped-horizontally-p flipped-vertically-p)
-	    (progn
-	      (setf real-position
-		    (vec2
-		     (+
-		      (- (x position))
-		      (- tile-width))
-		     (+
-		      (- (y position))
-		      (- tile-height))))
-	      (scale-canvas -1 -1))
-	    (if flipped-horizontally-p
-		(progn
-		  (setf real-position
-			(vec2
-			 (+
-			  (- (x position))
-			  (- tile-width))
-			 (y position)))
-		  (scale-canvas -1 1))
-		(if flipped-vertically-p
-		    (progn
-		      (setf real-position
-			    (vec2
-			     (x position)
-			     (+
-			      (- (y position))
-			      (- tile-height))))
-		      (scale-canvas 1 -1)))))
-	(draw-image
-	 real-position
-	 tileset-image
-	 :origin position-on-tileset
-	 :width tile-width
-	 :height tile-height)))))
+        (if (and flipped-horizontally-p flipped-vertically-p)
+            (progn
+              (setf real-position
+                    (vec2
+                     (+
+                      (- (x position))
+                      (- tile-width))
+                     (+
+                      (- (y position))
+                      (- tile-height))))
+              (scale-canvas -1 -1))
+            (if flipped-horizontally-p
+                (progn
+                  (setf real-position
+                        (vec2
+                         (+
+                          (- (x position))
+                          (- tile-width))
+                         (y position)))
+                  (scale-canvas -1 1))
+                (if flipped-vertically-p
+                    (progn
+                      (setf real-position
+                            (vec2
+                             (x position)
+                             (+
+                              (- (y position))
+                              (- tile-height))))
+                      (scale-canvas 1 -1)))))
+        (draw-image
+         real-position
+         tileset-image
+         :origin position-on-tileset
+         :width tile-width
+         :height tile-height)))))
