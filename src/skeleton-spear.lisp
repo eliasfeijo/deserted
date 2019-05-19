8ff079f278d59d758abf3ea59531d000f0efc67a
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-05-19 10:47:57 -0300

    state slot. 'buried -> 'rising logic

--- src/skeleton-spear.lisp
+++ src/skeleton-spear.lisp
@@ -5,10 +5,15 @@
   ((velocity :initform (vec2 100 100))
    (size :initform (vec2 64 64))
    (vision-range :initform 50)
-   (aggro :initform nil)))
+   (aggro :initform nil)
+   (state :initform 'buried)
+   (state-started :initform (real-time-seconds))
+   (current-animation :initform *skeleton-buried*)))
 
 (defun update-skeleton-spear (skeleton world)
-  (with-slots (position size vision-range aggro) skeleton
+  (with-slots (position size vision-range aggro
+                        state state-started current-animation)
+      skeleton
     (let ((vision-rect (vec4
                         (- (x position) vision-range)
                         (- (y position) vision-range)
@@ -20,12 +25,17 @@
                         (x (size-of (player-of world)))
                         (y (size-of (player-of world))))))
       (if (intersect-p vision-rect player-rect)
-          (setf aggro t)))))
+          (setf aggro t))
+      (when aggro
+        (cond ((eql state 'buried)
+                 (setf state 'rising
+                     state-started (real-time-seconds)
+                     current-animation *skeleton-rise*)
+               (start-animation current-animation state-started)))))))
 
 (defmethod render ((this skeleton-spear))
-  (with-slots (position size vision-range aggro) this
-    (let* ((anim *skeleton-buried*)
-           (frame (get-frame anim (real-time-seconds)))
+  (with-slots (position size vision-range aggro current-animation) this
+    (let* ((frame (get-frame current-animation (real-time-seconds)))
            (origin (keyframe-origin frame))
            (end (keyframe-end frame)))
       (draw-image position 'skeleton-spear
